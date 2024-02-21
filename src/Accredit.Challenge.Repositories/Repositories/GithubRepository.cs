84217e1f2900423df6f0614f4fe236fbe346cc93
Matheus Rizzo <matheusr032009@hotmail.com>
2024-02-21 17:50:51 -0300

    Create github repository

---
+++ src/Accredit.Challenge.Repositories/Repositories/GithubRepository.cs
@@ -0,0 +1,20 @@
+﻿using Accredit.Challenge.Borders.Dtos;
+using Accredit.Challenge.Borders.Repositories;
+using Accredit.Challenge.Repositories.HttpClients;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Accredit.Challenge.Repositories.Repositories
+{
+    public class GithubRepository : IGithubRepository
+    {
+        private readonly GithubHttpClient _githubHttpClient;
+        public GithubRepository(GithubHttpClient githubHttpClient)
+        {
+            _githubHttpClient = githubHttpClient;
+        }
+
+        public async Task<GithubUserDto> GetUser(string username) => await _githubHttpClient.GetUser(username);
+        public async Task<IEnumerable<GithubRepoDto>> GetUserRepos(string username) => await _githubHttpClient.GetUserRepos(username);
+    }
+}
